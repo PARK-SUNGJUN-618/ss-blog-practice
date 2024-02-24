33bcc39f273ce90b919ab9024686716c6c76fe1b
Sungjun Park <espoir618@gmail.com>
2024-02-24 17:56:00 +0900

    240224 Jun Get postInfo Using getStaticProps in nextjs environment

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,23 +1,42 @@
+import { GetStaticProps, NextPage } from "next";
+import { useEffect, useState } from "react";
 import BlogCard from "@/components/BlogCard";
-import { NextPage } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/posts").then((data) =>
+    data.json()
+  );
+  return {
+    props: { postInfo: res.postInfo },
+  };
+};
 
 interface Props {}
 
-const Blogs: NextPage<Props> = () => {
+const Blogs: NextPage<Props> = ({ postInfo }) => {
+  // // Getting postInfo in a React environment, not nextjs environment
+  // const [posts, setPosts] = useState<
+  //   {
+  //     title: string;
+  //     slug: string;
+  //     meta: string;
+  //   }[]
+  // >([]);
+  //
+  // const fetchPosts = async () => {
+  //   const res = await fetch("/api/posts").then((data) => data.json());
+  //   setPosts(res.postInfo);
+  // };
+  //
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, []);
+
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      <BlogCard
-        title="This is my blog"
-        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, rerum optio. Ullam quam vel aut culpa autem assumenda laboriosam iste!"
-      />
-      <BlogCard
-        title="This is my blog"
-        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, rerum optio. Ullam quam vel aut culpa autem assumenda laboriosam iste!"
-      />
-      <BlogCard
-        title="This is my blog"
-        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, rerum optio. Ullam quam vel aut culpa autem assumenda laboriosam iste!"
-      />
+      {postInfo.map((post, index) => (
+        <BlogCard key={index} title={post.title} desc={post.meta} />
+      ))}
     </div>
   );
 };
