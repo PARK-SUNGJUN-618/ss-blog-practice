8e3f379841e448887a92f3d8b1a1ea4850a8d602
Sungjun Park <espoir618@gmail.com>
2024-02-24 20:01:11 +0900

    240224 Jun Change to typescript source code and use Link

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,17 +1,24 @@
-import { GetStaticProps, NextPage } from "next";
-import { useEffect, useState } from "react";
+import { InferGetStaticPropsType, NextPage } from "next";
 import BlogCard from "@/components/BlogCard";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch("http://localhost:3000/api/posts").then((data) =>
-    data.json()
-  );
+interface PostApiResponse {
+  postInfo: {
+    title: string;
+    slug: string;
+    meta: string;
+  }[];
+}
+
+export const getStaticProps = async () => {
+  const { postInfo }: PostApiResponse = await fetch(
+    "http://localhost:3000/api/posts"
+  ).then((data) => data.json());
   return {
-    props: { postInfo: res.postInfo },
+    props: { postInfo: postInfo },
   };
 };
 
-interface Props {}
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const Blogs: NextPage<Props> = ({ postInfo }) => {
   // // Getting postInfo in a React environment, not nextjs environment
@@ -33,9 +40,14 @@ const Blogs: NextPage<Props> = ({ postInfo }) => {
   // }, []);
 
   return (
-    <div className="max-w-3xl mx-auto p-5 space-y-5">
-      {postInfo.map((post, index) => (
-        <BlogCard key={index} title={post.title} desc={post.meta} />
+    <div className="max-w-3xl mx-auto p-5 space-y-5 flex flex-col">
+      {postInfo.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
       ))}
     </div>
   );
