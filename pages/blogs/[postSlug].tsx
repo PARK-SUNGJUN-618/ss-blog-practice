e7bd03aac14411c623e55664193fa66855c65928
Sungjun Park <espoir618@gmail.com>
2024-02-25 14:35:18 +0900

    240225 Jun Returning All Paths

--- pages/blogs/[postSlug].tsx
+++ pages/blogs/[postSlug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
 
 interface Props {}
 
@@ -10,8 +13,17 @@ const SinglePage: NextPage<Props> = () => {
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
+  // reading paths
+  const dirPathToRead = path.join(process.cwd(), "posts");
+  const dirs = fs.readdirSync(dirPathToRead);
+  const paths = dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), "posts/" + filename);
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: "utf-8" });
+    return { params: { postSlug: matter(fileContent).data.slug } };
+  });
+
   return {
-    paths: [{ params: { postSlug: "clever-lazy-loading-for-javascript" } }],
+    paths,
     fallback: false, // we will come to this later and understand this with example.
   };
 };
